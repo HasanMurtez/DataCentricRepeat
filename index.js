bb2527647ae61daeb65ec7f5ccae6a2e1cd672cc
Hasan Murtaza <g00419888@atu.ie>
2025-08-10 14:44:33 +0000

    Home page with Students, Grades, and Lecturers links

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ var mysqlDAO = require('./mySqlDao');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3004, () => {
     console.log("Running on port 3004");
 });
 
 app.get("/", (req, res) => {
-    mysqlDAO.getStudents()
-    .then((data) => {
-        res.send(data);
-    })
-    .catch((error) => {
-        res.send(error);
-    });
+    res.render("home"); 
 });
